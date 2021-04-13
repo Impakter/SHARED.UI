696c1db7654629b6ed927e3f7da98a406a70d3c2
Aravind Samala <52457571+thyaravind@users.noreply.github.com>
2021-04-13 16:08:30 -0400

    initializing

---
+++ src/components/NewsArticle.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="article">
+<h3>{{articleTitle}}</h3>
+<div>{{articleImage}}</div>
+<slot></slot>
+</div>    
+</template>
+
+<script>
+export default {
+  name: 'NewsArticle',
+  props: {
+    articleTitle: String,
+    articleImage: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
